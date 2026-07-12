4bb079c03f7a1815c83a93bfac81bca160b26e70
hari2k7 <hari082007@gmail.com>
2026-07-12 09:57:07 +0530

    chore: initialize backend project structure and Express setup

--- server/src/db/database.js
+++ server/src/db/database.js
@@ -1,7 +1,7 @@
 import app from './app.js'
 import 'dotenv/config'
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, (req, res) => {
     console.log(`Server is running in port ${PORT}`);
