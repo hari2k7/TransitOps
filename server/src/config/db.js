1d4f95b38ade64938f405d253cc070ee120573e7
hari2k7 <hari082007@gmail.com>
2026-07-12 11:29:47 +0530

    chore: refractor db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,14 +1,24 @@
-import { Pool } from "pg";
+import pg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
 
+const { Pool } = pg;
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+});
+
+pool.on("connect", () => {
+    console.log("PostgreSQL Connected");
+});
+
+pool.on("error", (err) => {
+    console.error("Unexpected PostgreSQL Error", err);
 });
 
 export default pool;
